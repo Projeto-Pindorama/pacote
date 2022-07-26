ee4deadff55ddf726188da8efad4c096cdc788a1
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2022-07-26 05:46:10 -0300

    feat.: Implement a initial pfmt() funcion for SVID4-like error reporting.
    May not as simple as I though, but there is it!
    
    "Há muito tempo eu vivi calado
    Mas agora resolvi falar
    Chegou a hora, tem que ser agora
    E com você não posso mais ficar"

---
+++ internal/pfmt/pfmt.go
@@ -0,0 +1,45 @@
+/*
+* This contains a reimplementation of pfmt(), which is a tool for making
+* SVID4-style error reporting. In original Sun pkgtools, one would use progerr()
+* instead of pfmt(), although the messages looked identical at some point.
+* These pkgtools will use this pfmt() implementation instead of log.Fatal or
+* something like.
+* Temporary reference: http://www.sco.com/developers/devspecs/vol1a.pdf
+ */
+package pfmt
+
+import "fmt"
+
+func pfmt(stream Writer, report string, message ...string) {
+	fmt.Fprintf(stream, "%s: %s", reportType(report), fmt.Sprintf(message))
+}
+
+func reportType(report string) string {
+	switch report {
+	/* The default messages/report flags, nothing new  */
+	case MM_HALT:
+		return "HALT"
+	case MM_ERROR:
+		return "ERROR"
+	case MM_WARNING:
+		return "WARNING"
+	case MM_INFO:
+		return "INFO"
+	/* Action: this will print a string saying that something has to be
+	* fixed or done */
+	case MM_ACTION:
+		return "TO FIX"
+	/* Catalog access: I think this would be meant for gettext and
+	* i18n. Since we haven't implemented i18n yet, we don't have a specific
+	* way to tell "Hey, don't translate this error message, ok? Print it
+	* verbatim.", so I think this will be a no-op for now. */
+	case MM_GET:
+	case MM_NOGET:
+	/* These are used to set the output format. Basically, if "MM_NOSTD" is
+	* set, pfmt() shall work as a generic printf() funcion. I had an idea of
+	* how to implement this, but I fear breaking the rest, so I will keep as
+	* a no-op for now too. */
+	case MM_STD:
+	case MM_NOSTD:
+	}
+}

347a0a978583ee61515f93a1e2f0072c801a6b81
Luiz Antônio <luiz.antonio.rangel@outlook.com.br>
2024-08-02 17:06:27 -0300

    chore: pkgproto: small refactor, added copyright header.

--- cmd/pkgproto/main.go
+++ cmd/pkgproto/main.go
@@ -1,3 +1,15 @@
+/*
+ * pkgproto - generate a prototype file
+ */
+/*
+ * Copyright (C) 2022 - 2024: Luiz Antônio Rangel (takusuman)
+ * 				Samuel Brederodes (callsamu)
+ *				João Pedro Vieira (JoaoP-Vieira)
+ *
+ *
+ * SPDX-Licence-Identifier: NCSA 
+ */
+
 package main
 
 import (
@@ -10,42 +22,49 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/Projeto-Pindorama/motoko/internal/archivum"
+	"github.com/Projeto-Pindorama/pacote/internal/archivum"
 )
 
+var progname string
 var class string
-var ignoreLinks bool
+var fIgnoreLinks bool
 
 func main() {
-	flag.StringVar(&class, "c", "none", "Maps the class of all paths to class.")
-	flag.BoolVar(&ignoreLinks, "i", false, "Ignores symbolic links and records the paths as ftype=f (a file) versus ftype=s (symbolic link).")
+	progname = os.Args[0]
+	var remainingArgs []string
+	var unixFS archivum.UnixFS
 
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "usage: pkgproto [-i] [-c class] [path ...]\n")
-	}
-
+	/* Argument parser. */
+	flag.StringVar(&class, "c", "none",
+		"Maps the class of all paths to class.")
+	flag.BoolVar(&fIgnoreLinks, "i", false, `Ignores symbolic links and records
+the paths as ftype=f (a file) versus
+ftype=s (symbolic link).`)
+	flag.Usage = usage
 	flag.Parse()
+	remainingArgs = flag.Args()
 
-	remainingArgs := flag.Args()
-
-	unixFS := archivum.NewUnixFS("/")
-
+	unixFS = archivum.NewUnixFS("/")
 	for _, v := range remainingArgs {
 		dir := strings.SplitN(v, "=", 2)
 
-		filepath.WalkDir(dir[0], func(path string, d fs.DirEntry, err error) error {
+		filepath.WalkDir(dir[0],
+			func(path string, d fs.DirEntry, err error) error {
+			var metadata *archivum.Metadata
+
 			if err != nil {
 				log.Fatal(err)
 				return err
 			}
 
-			metadata, err := archivum.Scan(unixFS, path)
+			metadata, err = archivum.Scan(unixFS, path)
 			if err != nil {
 				log.Fatal(err)
 				return err
 			}
 
-			fmt.Println(strings.Replace(MetadataToString(metadata), dir[0], dir[1], 1))
+			fmt.Println(strings.Replace(MetadataToString(metadata),
+								dir[0], dir[1], 1))
 
 			return nil
 		})
@@ -69,21 +88,32 @@ func main() {
 
 func MetadataToString(m *archivum.Metadata) string {
 	var ret string
-	var fileType rune = m.FType
+	var fileType rune
+	fileType = m.FType
 
-	if m.FType == 's' && ignoreLinks {
+	if fileType == 's' && fIgnoreLinks {
 		fileType = 'f'
 	}
 
-	ret = fmt.Sprintf("%c %s %s", fileType, class, m.Path)
+	ret = fmt.Sprintf("%c %s %s",
+			fileType, class, m.Path)
 
 	if fileType == 's' {
 		ret = ret + fmt.Sprintf("=%s", m.RealPath)
 	} else if m.DeviceInfo == nil {
-		ret = ret + fmt.Sprintf(" %s %s %s", m.OctalMod, m.Owner, m.Group)
+		ret = ret + fmt.Sprintf(" %s %s %s",
+					m.OctalMod, m.Owner, m.Group)
 	} else {
-		ret = ret + fmt.Sprintf(" %d %d %s %s %s", m.DeviceInfo.Major, m.DeviceInfo.Minor, m.OctalMod, m.Owner, m.Group)
+		ret = ret + fmt.Sprintf(" %d %d %s %s %s", 
+				m.DeviceInfo.Major, m.DeviceInfo.Minor, m.OctalMod,
+				m.Owner, m.Group)
 	}
 
 	return ret
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr,
+		"usage: %s [-i] [-c class] [path ...]\n",
+			progname)
 }
